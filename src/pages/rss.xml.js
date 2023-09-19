7bc7c44e0ac6d351909bbbd9c5389308cc02ffb1
Antonio_Garrido <99376135+AressAntonio@users.noreply.github.com>
2023-09-18 19:49:53 -0600

    Cambios de estilo en home and blog

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,8 +3,8 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 export async function get() {
   return rss({
     title: 'AressAntonio | Blog',
-    description: 'Mi viaje de aprendizaje de Astro',
-    site: 'https://my-blog-site.netlify.app',
+    description: 'Mi viaje en el mundo de la programacion',
+    site: 'https://aressantonio-littleblog.netlify.app',
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>en-es</language>`,
   });
